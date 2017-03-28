93b2856ea6b51bb9c722ac51ab1208b6b49b2097
J3ph42 <j3ph.2000@gmail.com>
2017-03-28 10:44:41 -0400

    More minor cleanup, changed some variable names & formatting

--- GetPCInfo.ps1
+++ GetPCInfo.ps1
@@ -1,4 +1,4 @@
-﻿function getPCInfo
+function getPCInfo
 {
 Param($PCNAME)
     # I want some short neames to refer to these WMI objects by
@@ -20,8 +20,8 @@ Param($PCNAME)
     $OS_Version = $OS.Version
     $User = $CS.UserName
     $RAM = ($CS.TotalPhysicalMemory/1MB).ToString(".")
-    $Up_Time = (Get-Date) - ($Last_Boot)
-    $Up_Time_Formatted = "Uptime: " + $Up_Time.Days + " days, " + $Up_Time.Hours + " hours, " + $Up_Time.Minutes + " minutes" 
+    $UpTime = (Get-Date) - ($Last_Boot)
+    $UpTime_Formatted = "Uptime: " + $UpTime.Days + " days, " + $UpTime.Hours + " hours, " + $UpTime.Minutes + " minutes" 
 
     # And try to find out what OU the PC is in.
     Import-Module -Name ActiveDirectory -Cmdlet Get-ADComputer, Get-ADOrganizationalUnit;
@@ -30,26 +30,27 @@ Param($PCNAME)
 
     # Display minimally formatted output
     Write-Output "Note: some info may not populate if this is run against the local PC"
-    Write-Output "-------------------------------------"
+    Write-Output "------------------------------------------------"
     Write-Output "$User is logged in"
-    Write-Output "$Up_Time_Formatted"
-    Write-Output "-------------------------------------"
+    Write-Output "$UpTime_Formatted"
+    Write-Output "------------------------------------------------"
     Write-Output "Hostname: $Server_Name"
     Write-Output "$OU"
-    Write-Output "$OS_Name| $OS_Arch | $OS_Version"
-    Write-Output "-------------------------------------"
+    Write-Output "------------------------------------------------"
+    Write-Output "$OS_Name $OS_Arch Version $OS_Version"
+    Write-Output "------------------------------------------------"
     Write-Output "RAM:$RAM MB"
-
-    Write-Output "-------------------------------------"
+    Write-Output "------------------------------------------------"
     Write-Output "OEM Serial #: $Serial_Number"
     Write-Output "BIOS:$BIOS_Version"
     Write-Output "SMBIOS:$SMBIOSV"
     Write-Output "GUID: $SMBIOS_GUID"
+    Write-Output "------------------------------------------------"
 
     # We need more than a nanosecond to read this, so we want it to pause before closing.
     Read-Host -Prompt "Press Enter to exit"
 }
 
-$PC_AD_Name=read-host "Enter computer name:"
+$PC_HostName=read-host "Enter computer name:"
 
-getPCInfo $PC_AD_Name
+getPCInfo $PC_HostName
